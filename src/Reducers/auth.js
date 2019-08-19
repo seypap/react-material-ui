fb34baa50b47b1a9bcd13e9f8da1dcde4fd81074
seyran_papyan <seyran.papyan95@outlook.com>
2019-08-18 22:27:41 -0300

    change auth reducer

--- src/Reducers/auth.js
+++ src/Reducers/auth.js
@@ -1,4 +1,6 @@
 export const LOG_IN = 'LOG_IN';
+export const SIGN_IN = 'SIGN_IN';
+
 export const LOG_IN_SUCCESS = 'LOG_IN_SUCCESS';
 export const LOG_IN_FAILURE = 'LOG_IN_FAILURE';
 export const LOG_OUT = 'LOG_OUT';
